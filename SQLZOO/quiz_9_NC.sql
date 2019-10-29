cbc866c136d6e5c407fddaf30bd226f6be811ead
Sebastian <sgrygorczuk@gmail.com>
2019-10-28 20:33:37 -0400

    Added Lesson #9

---
+++ SQLZOO/quiz_9_NC.sql
@@ -0,0 +1,17 @@
+/*
+SQLZOO - https://sqlzoo.net/wiki/SQL_Tutorial
+Learn SQL using: SQL Server, Oracle, MySQL, DB2, and PostgreSQL.
+*/
+
+/*
+Quiz #9:
+stops	route
+id	num
+name	company
+	pos
+	stop
+*/
+
+/*
+Q1. Select the code that would show it is possible to get from Craiglockhart to Haymarket 
+*/
